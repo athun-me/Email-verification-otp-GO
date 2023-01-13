527d027a41f790e501b47ba5d8186f5f85de5c80
Athunlal <athunlalp@gmail.com>
2023-01-13 19:45:46 +0530

    finished

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"net/smtp"
+
+	// "strings"
+
+	"math/big"
+	"strconv"
+)
+
+func main() {
+	otp, err := getRandNum()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	sendMail(otp)
+}
+
+func sendMail(otp string) {
+
+	// Sender data.
+	from := "golangathunbrototype@gmail.com"
+	password := "cpgmhxygkwpirrqi"
+
+	// Receiver email address.
+	to := []string{
+		"athunlalp@gmail.com",
+	}
+
+	// smtp server configuration.
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+
+	// Message.
+
+	// Authentication.
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// Sending email.
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, []byte(otp+" is your otp"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Email Sent Successfully!")
+}
+
+func getRandNum() (string, error) {
+	nBig, e := rand.Int(rand.Reader, big.NewInt(8999))
+	if e != nil {
+		return "", e
+	}
+	return strconv.FormatInt(nBig.Int64()+1000, 10), nil
+}
